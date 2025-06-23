58be83f19c47a666bf94d0a14fe49f890a99e25f
anyduene <leosmile06@gmail.com>
2025-06-23 16:39:06 +0300

    chore(api): enable CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,14 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:4200'
+  });
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
@@ -18,4 +18,4 @@ async function bootstrap() {
   );
 }
 
-bootstrap();
+bootstrap().then();
