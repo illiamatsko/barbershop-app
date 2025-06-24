2da53b2baa9909c9f6669a9bf337e8efba6f1f9b
anyduene <leosmile06@gmail.com>
2025-06-24 23:57:27 +0300

    feat(client): create Header component

--- apps/client/src/app/app.ts
+++ apps/client/src/app/app.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { Header } from './shared/components/header/header';
 
 @Component({
-  imports: [RouterModule],
+  imports: [RouterModule, Header],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
---
+++ apps/client/src/app/shared/components/header/header.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Header {}
