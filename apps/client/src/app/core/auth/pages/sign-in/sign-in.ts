fada706c9edd274fb0858a1a6c1223691a6a3f29
anyduene <leosmile06@gmail.com>
2025-06-26 14:02:33 +0300

    feat(auth): enhance SignIn component with validation and styles

--- apps/client/src/app/core/auth/pages/sign-in/sign-in.ts
+++ apps/client/src/app/core/auth/pages/sign-in/sign-in.ts
@@ -1,11 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-in',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SignIn {}
+export class SignIn {
+  showError = signal<boolean>(false);
+  errorMessage = signal<string>('');
+
+  signInForm = new FormGroup(
+    {
+      email: new FormControl<string>('', {
+        validators: [Validators.required, Validators.email],
+      }),
+      password: new FormControl('', {
+        validators: [Validators.required, Validators.minLength(6)],
+      }),
+    },
+    {
+      updateOn: 'blur'
+    }
+  );
+
+  getError(errorCode: string, control: AbstractControl) {
+    if (!control) return false;
+    return control.touched && control.dirty && control.hasError(errorCode);
+  }
+
+  onSubmit() {
+    if (this.signInForm.invalid) {
+      return;
+    }
+
+    const { email, password } = this.signInForm.getRawValue();
+
+    console.log(email, password);
+  }
+}
