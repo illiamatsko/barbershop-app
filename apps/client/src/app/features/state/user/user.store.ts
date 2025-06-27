7fb6e24c41c316fbc54af415ab1c1d98ed88deca
anyduene <leosmile06@gmail.com>
2025-06-27 23:18:56 +0300

    feat(client): add isSignedIn computed to User store and update header UI

--- apps/client/src/app/features/state/user/user.store.ts
+++ apps/client/src/app/features/state/user/user.store.ts
@@ -1,11 +1,15 @@
-import { patchState, signalStore, withMethods, withState } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
 import { userInitialSlice, userSlice } from './user.slice';
 import { setUser } from './user.updaters';
+import { computed } from '@angular/core';
 
 export const UserStore = signalStore(
   { providedIn: 'root' },
   withState<userSlice>(userInitialSlice),
-  withMethods(store => ({
-    setUser: (res: userSlice) => patchState(store, setUser(res))
+  withMethods((store) => ({
+    setUser: (res: userSlice) => patchState(store, setUser(res)),
+  })),
+  withComputed((store) => ({
+    isSignedIn: computed(() => !!store.token()),
   }))
-)
+);
--- apps/client/src/app/shared/components/header/header.ts
+++ apps/client/src/app/shared/components/header/header.ts
@@ -1,16 +1,20 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { UserStore } from '../../../features/state/user/user.store';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
   private router = inject(Router);
+  private userStore = inject(UserStore);
+
+  isSignedIn = computed(() => this.userStore.isSignedIn());
 
   navigateToSignIn() {
     this.router.navigate(['auth/sign-in']).then();
