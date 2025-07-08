e56006c74cfb39e39c7ab983eae2af9c4bcb6b3e
anyduene <leosmile06@gmail.com>
2025-07-08 17:43:08 +0300

    feat(barbers): add Barbers section to landing page and implement data fetching

---
+++ libs/client/layout/presentation/barbers-section/barbers-section.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angular/core';
+import { BarberDto } from '@barbershop-app/shared/types';
+import { GetAllBarbersUseCase } from '@barbershop-app/client/barber/application';
+
+
+@Component({
+  selector: 'app-barbers-section',
+  imports: [],
+  templateUrl: './barbers-section.html',
+  styleUrl: './barbers-section.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BarbersSection implements OnInit {
+  private getAllBarbersUseCase = inject(GetAllBarbersUseCase);
+  barbers = signal<BarberDto[]>([])
+
+  async ngOnInit() {
+    this.barbers.set(await this.getAllBarbersUseCase.execute());
+  }
+}
--- libs/client/layout/presentation/landing/landing.ts
+++ libs/client/layout/presentation/landing/landing.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroSection } from '../hero-section/hero-section';
 import { AboutSection } from '../about-section/about-section';
+import { BarbersSection } from '../barbers-section/barbers-section';
 
 @Component({
   selector: 'app-landing',
-  imports: [CommonModule, HeroSection, AboutSection],
+  imports: [CommonModule, HeroSection, AboutSection, BarbersSection],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
