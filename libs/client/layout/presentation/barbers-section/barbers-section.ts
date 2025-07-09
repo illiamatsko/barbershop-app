84f80d943f9838a5bc903f6055245e561b6e597a
anyduene <leosmile06@gmail.com>
2025-07-09 22:02:45 +0300

    feat(barbers): implement Barbers section with styling and dynamic barber cards

--- libs/client/layout/presentation/barbers-section/barbers-section.ts
+++ libs/client/layout/presentation/barbers-section/barbers-section.ts
@@ -1,11 +1,19 @@
-import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  inject,
+  OnInit,
+  signal
+} from '@angular/core';
 import { BarberDto } from '@barbershop-app/shared/types';
 import { GetAllBarbersUseCase } from '@barbershop-app/client/barber/application';
+import { BarberCard } from './barber-card/barber-card';
 
 
 @Component({
   selector: 'app-barbers-section',
-  imports: [],
+  imports: [BarberCard],
   templateUrl: './barbers-section.html',
   styleUrl: './barbers-section.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -13,7 +21,7 @@ import { GetAllBarbersUseCase } from '@barbershop-app/client/barber/application'
 })
 export class BarbersSection implements OnInit {
   private getAllBarbersUseCase = inject(GetAllBarbersUseCase);
-  barbers = signal<BarberDto[]>([])
+  barbers = signal<BarberDto[]>([]);
 
   async ngOnInit() {
     this.barbers.set(await this.getAllBarbersUseCase.execute());
